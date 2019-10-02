8d9d3a866246ff9c23c1548469f4691f17f8d59a
Jay Cuthrell <jay@cuthrell.com>
2019-10-02 17:28:12 -0500

    color

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
   manifestName: 'cuthrell.org',
   manifestShortName: 'The Cuthrell Foundation', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
+  manifestBackgroundColor: '#efefef',
+  manifestThemeColor: '#efefef',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
