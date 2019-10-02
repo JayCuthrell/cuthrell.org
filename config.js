3d80984bca802a7dec01fc03cc86b11ddd1bea9d
Jay Cuthrell <jay@cuthrell.com>
2019-10-02 17:13:36 -0500

    basic edits

--- config.js
+++ config.js
@@ -1,37 +1,37 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Aerial', // <title>
-  manifestName: 'Aerial',
-  manifestShortName: 'Portfolio', // max 12 characters
+  siteTitle: 'cuthrell.org', // <title>
+  manifestName: 'cuthrell.org',
+  manifestShortName: 'The Cuthrell Foundation', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-aerial/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   // social
 
-  authorName: 'Anubhav Srivastava',
-  authorHeading: 'Web Developer - JAMStack - Open Source enthusiast',
+  authorName: 'Jay Cuthrell',
+  authorHeading: 'Administrator',
   authorSocialLinks: [
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: '',
     },
     {
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
+      url: '',
     },
     {
       icon: 'fa-facebook',
       name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      url: '',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'jay@cuthrell.com',
     },
   ],
 };
